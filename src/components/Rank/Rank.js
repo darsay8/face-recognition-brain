19b4127c12fe5f37fbcfc63b824cdddf74e38797
darsay8 <darsay.one@gmail.com>
2019-01-22 11:19:25 -0300

    first commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+            {'Rob, your current rank is...'}
+            </div>
+
+            <div className='white f1'>
+                {'#1'}
+            </div>
+
+
+        </div>
+    )
+
+};
+
+
+export default Rank;
