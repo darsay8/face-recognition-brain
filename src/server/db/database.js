3ef3e24732cf1c1d41088ec67674b4eab549ad21
darsay8 <darsay.one@gmail.com>
2021-09-24 22:47:14 -0300

    database prod

--- src/server/db/database.js
+++ src/server/db/database.js
@@ -2,15 +2,26 @@ const knex = require('knex')
 require('dotenv').config()
 
 const { env } = process
+let db = ''
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASS,
-    database: env.DB_NAME,
-  },
-})
+if (env.NODE_ENV !== 'production') {
+  db = knex({
+    client: 'pg',
+    connection: {
+      host: env.DB_HOST,
+      user: env.DB_USER,
+      password: env.DB_PASS,
+      database: env.DB_NAME,
+    },
+  })
+} else {
+  db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+  })
+}
 
 module.exports = db
